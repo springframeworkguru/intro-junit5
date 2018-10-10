9887d73d785d2bdd6a9e010d9a7570223f3c6977
John Thompson <john@springframework.guru>
2018-10-10 18:56:46 -0400

    Init Project Add

---
+++ src/main/java/guru/springframework/Greating.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+/**
+ * Created by jt on 10/10/18.
+ */
+public class Greating {
+
+    private static final String HELLO = "Hello";
+    private static final String WORLD = "World";
+
+    public String helloWorld(){
+        return HELLO + " " + WORLD;
+    }
+
+    public String helloWorld(String name){
+        return HELLO + " " + name;
+    }
+}

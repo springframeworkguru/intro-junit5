873a93ae1a0aed4b52c61758d27edbb18e3c8404
springframeworkguru <john@springframework.guru>
2019-02-16 13:52:37 +0530

    Removed redundant Greeting instantiation in GreetingTest

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -10,22 +10,16 @@ class GreetingTest {
     @BeforeEach
     void setUp() {
         System.out.println("In Before Each....");
-
         greeting = new Greeting();
     }
 
     @Test
     void helloWorld() {
-        greeting = new Greeting();
-
         System.out.println(greeting.helloWorld());
-
     }
 
     @Test
     void helloWorld1() {
-        greeting = new Greeting();
-
         System.out.println(greeting.helloWorld("John"));
     }
 }
